c857fddf975e6a16412979bbc0e9f96582a03283
zx <zhouxi@Mac-Studio.local>
2024-11-13 21:06:43 +0800

    [feat]发布订阅基本实现

--- src/handwrittenCode/EventEmitter/index.ts
+++ src/handwrittenCode/EventEmitter/index.ts
@@ -3,19 +3,64 @@
 /** --- 监听器
  * 实现：on once emit off 以及订阅中心 Map<事件名, 事件处理函数[]>
  */
-class Emitter implements IEmitter {
-  evtMap: Map<string, Function[]>
+class Emitter implements EventEmitter {
+  evtMap: Map<string, EventHandler[]>
   constructor() {
     this.evtMap = new Map()
   }
 
-  on() {}
+  on(evtName: string, handler: EventHandler) {
+    const handlerArr = this.evtMap.get(evtName) ?? []
+    handlerArr.push(handler)
+    this.evtMap.set(evtName, handlerArr)
+  }
 
-  once() {}
+  once(evtName: string, handler: EventHandler) {
+    /**  --- 第一种 */
+    // const onceHandler = function (...args: Parameters<EventHandler>) {
+    //   let isCalled = false
+    //   return () => {
+    //     if (!isCalled) {
+    //       handler.apply(this, args)
+    //       isCalled = true
+    //     }
+    //   }
+    // }
+    // const handlerArr = this.evtMap.get(evtName) ?? []
+    // handlerArr.push(onceHandler)
+    // this.evtMap.set(evtName, handlerArr)
+    /**  --- 第二种 */
+    const onceHandler = (...args: any[]) => {
+      handler(...args)
+      this.off(evtName, onceHandler)
+    }
+    this.on(evtName, onceHandler)
+  }
 
-  emit() {}
+  emit(evtName: string, ...args: any[]) {
+    const handlerArr = this.evtMap.get(evtName) ?? []
+    if (!handlerArr.length) return
+    handlerArr.forEach(handler => {
+      handler(...args)
+    })
+  }
 
-  off() {}
+  /**
+   * off
+   * @description 取消事件监听
+   * @param {string} evtName 事件名
+   * @param {EventHandler} [handler] 事件处理函数，为空时删除 evtName 事件所有处理函数
+   */
+  off(evtName: string, handler?: EventHandler) {
+    if (!handler) {
+      this.evtMap.delete(evtName)
+      return
+    }
+    const handlerArr = this.evtMap.get(evtName) ?? []
+    if (!handlerArr.length) return
+    handlerArr.splice(handlerArr.indexOf(handler) >>> 0, 1)
+    this.evtMap.set(evtName, handlerArr)
+  }
 }
 
 const emitter = new Emitter()
--- src/handwrittenCode/EventEmitter/type.ts
+++ src/handwrittenCode/EventEmitter/type.ts
@@ -1,7 +1,11 @@
-interface IEmitter {
+interface EventHandler {
+  (...args: any[]): void
+}
+
+interface EventEmitter {
   evtMap: Map<string, Function[]>
-  on: (evtName: string, handler: (data?: any) => void) => void // 订阅某事件
-  once: (evtName: string, handler: (data?: any) => void) => void // 仅触发一次的订阅
+  on: (evtName: string, handler: EventHandler) => void // 订阅某事件
+  once: (evtName: string, handler: EventHandler) => void // 仅触发一次的订阅
   emit: (evtName: string, ...args: any[]) => void // 派发事件
-  off: (evtName: string, handler?: Function) => void // 移除某事件的指定或全部监听器
+  off: (evtName: string, handler?: EventHandler) => void // 移除某事件的指定或全部监听器
 }
