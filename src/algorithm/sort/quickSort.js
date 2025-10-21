baaa44c38743c9daaeea0f47f40ac2e7fac2ea58
zx <zhouxi_aimee@163.com>
2025-10-21 16:05:33 +0800

    perf(快排)：优化快排 partition 哨兵划分

--- src/algorithm/sort/quickSort.js
+++ src/algorithm/sort/quickSort.js
@@ -11,6 +11,16 @@ const swap = require('./swap')
  * @param {Number[]} nums 待排序的数组
  */
 function quickSort(nums, left = 0, right = nums.length - 1) {
+  // 已排序 nums 直接返回
+  let isSorted = true
+  for (let i = left; i < right; i++) {
+    if (nums[i] > nums[i + 1]) {
+      isSorted = false
+      break
+    }
+  }
+  if (isSorted) return
+
   // 递归终止条件
   // if (left >= right) return
   while (left < right) {
@@ -29,38 +39,76 @@ function quickSort(nums, left = 0, right = nums.length - 1) {
   }
 }
 
-// 哨兵划分
+/** NOTE: 哨兵划分 第一版 */
+// function partition(nums, left, right) {
+//   let med = medianThree(nums, left, right) // 中位数下标
+//   swap(nums, left, med)
+//   // 以 left 下标对应的值作为基准数（NOTE: 交换后已经是首尾中三数中的中位数了，降低时间复杂度劣化风险）
+//   let i = left,
+//     j = right
+//   // NOTE: 当我们以最左端元素为基准数时，必须先“从右往左”再”从左往右“，原因是：先向前移动 j，再向后移动 i，这能保证两指针相遇时指向的一定是小于哨兵节点值的数，这才能在循环结束时交换相遇节点和哨兵节点，保证排序次序
+//   while (i < j) {
+//     while (i < j && nums[j] >= nums[left]) j--
+//     while (i < j && nums[i] <= nums[left]) i++
+//     swap(nums, i, j)
+//   }
+//   swap(nums, left, j)
+//   return j
+// }
+
+/** NOTE: 哨兵划分 第二版 更优解（对有大量重复数据的样例更友好） */
 function partition(nums, left, right) {
-  let med = medianThree(nums, left, right) // 中位数下标
-  swap(nums, left, med)
-  // 以 left 下标对应的值作为基准数（NOTE: 交换后已经是首尾中三数中的中位数了，降低时间复杂度劣化风险）
-  let i = left,
+  let idx = left + Math.floor(Math.random() * (right - left + 1))
+  const pivot = nums[idx]
+  swap(nums, left, idx)
+
+  // 2. 相向双指针遍历子数组 [left + 1, right]
+  // 循环不变量：在循环过程中，子数组的数据分布始终如下图
+  // [ pivot | <=pivot | 尚未遍历 | >=pivot ]
+  //   ^                 ^     ^         ^
+  //   left              i     j         right
+
+  let i = left + 1,
     j = right
-  // NOTE: 当我们以最左端元素为基准数时，必须先“从右往左”再”从左往右“，原因是：先向前移动 j，再向后移动 i，这能保证两指针相遇时指向的一定是小于哨兵节点值的数，这才能在循环结束时交换相遇节点和哨兵节点，保证排序次序
-  while (i < j) {
-    while (i < j && nums[j] >= nums[left]) j--
-    while (i < j && nums[i] <= nums[left]) i++
+  while (true) {
+    while (i <= j && nums[i] < pivot) i++
+    // 此时 nums[i] >= pivot
+
+    while (i <= j && nums[j] > pivot) j--
+    // 此时 nums[j] <= pivot
+
+    if (i >= j) break
+
+    // 维持循环不变量
     swap(nums, i, j)
+    i++
+    j--
   }
-  swap(nums, left, j)
+
+  // 循环结束后
+  // [ pivot | <=pivot | >=pivot ]
+  //   ^             ^   ^     ^
+  //   left          j   i     right
+
+  swap(nums, left, j) // 这里只能与 j 交换
   return j
 }
 
 // NOTE: 优化策略一：优化基准数选取（增大随机性，避免逆序数组劣化风险）
 // 求首尾中三数中的中位数的下标
-function medianThree(
-  nums,
-  left,
-  right,
-  mid = Math.floor(left + (right - left) / 2)
-) {
-  const l = nums[left],
-    r = nums[right],
-    m = nums[mid]
-  if ((l > m) ^ (l > r)) return left
-  if ((r > l) ^ (r > m)) return right
-  return mid
-}
+// function medianThree(
+//   nums,
+//   left,
+//   right,
+//   mid = Math.floor(left + (right - left) / 2)
+// ) {
+//   const l = nums[left],
+//     r = nums[right],
+//     m = nums[mid]
+//   if ((l > m) ^ (l > r)) return left
+//   if ((r > l) ^ (r > m)) return right
+//   return mid
+// }
 // TEST: 求三数中位数测试用例
 // const nums1 = [4, 26, 67]
 // console.log("==> medianThree", medianThree(nums1, 0, 2))
